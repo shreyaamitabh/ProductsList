f67feaf69bc986c4c5fff7357af1fa228642dc3b
Shreya <shreya.amitabh@gmail.com>
2020-06-22 21:49:45 +0530

    fourth

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,7 @@ class Header extends React.Component {
     return (
       <Fragment>
         <Navbar id="firstNav" sticky="top">
-          {/* <img id="navImage" src="https://thumbs.dreamstime.com/z/grocery-store-delivery-company-s-logo-items-vegetables-home-icon-vector-design-template-167341513.jpg"/>
-                    <div className="topnav-right">
-                    <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR2GQC84QrrCIBfaN1krHyjqGtNx02bji5kCQaO6iIBqYAjX9Pl&usqp=CAU" id="userNav" /> */}
-          {/* </div> */}
+          
         </Navbar>
       </Fragment>
     );
